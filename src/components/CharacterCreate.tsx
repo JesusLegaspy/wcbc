a1d2964aa06100df8f47ff9f0cd54081ef538e2d
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-09-17 18:41:16 -0500

    create character button sytles

--- src/components/CharacterCreate.tsx
+++ src/components/CharacterCreate.tsx
@@ -1,19 +1,22 @@
 import NavbarSub from "./NavbarSub";
 import CharacterList from './CharacterList';
 import { PiPlusSquareFill } from "react-icons/pi";
+import '../styles/CharacterCreate.css';
 
 const CharacterCreate = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
       <NavbarSub />
-      <div className=" container m-auto max-w-screen-xl flex-grow relative">
+      <div className="container m-auto max-w-screen-xl flex-grow relative">
         <CharacterList />
       </div>
-      <div className="relative container m-auto max-w-screen-xl pb-4">
-        <button className="fixed bottom-7 right-7 text-4xl ml-auto">
-          <PiPlusSquareFill />
-        </button>
+      <div className="relative container m-auto max-w-screen-xl pb-4 bg-blue-400">
+        <div className="relative container m-auto max-w-screen-xl pb-4">
+          <button className="btn-create-character fixed bottom-7 right-7 text-4xl ml-auto">
+            <PiPlusSquareFill />
+          </button>
+        </div>
       </div>
     </div>
   );
