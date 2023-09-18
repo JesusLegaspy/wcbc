a0a1f809d7498c58da45f5f771f1df9ebeed3ed5
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-09-18 18:11:06 -0500

    Typescript allow images import

--- src/components/CharacterCreate.tsx
+++ src/components/CharacterCreate.tsx
@@ -1,11 +1,27 @@
 import NavbarSub from "./NavbarSub";
+import profile from "../assets/images/profile.png";
 
 const CharacterCreate = () => {
 
   return (
     <div>
       <NavbarSub />
-      CharacterCreate
+      <div className="container m-auto max-w-screen-xl">
+        <form className="flex flex-col">
+          <div >
+            <img className="" src={profile} alt="Empty profile." />
+          </div>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div>
+            <label htmlFor="description">Last name:</label>
+            <input type="text" id="description" name="description" />
+          </div>
+        </form>
+      </div>
+
     </div>
   );
 }
