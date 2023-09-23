ade57f0fc7c0d076f29b9770f877dc40b2b831ed
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-09-23 16:59:48 -0500

    Expand details on selected Character

--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -12,11 +12,13 @@ const CharacterList = () => {
     } else {
       setExpandedCharacterId(characterId);
     }
-  }
+  };
 
-  const content = characters?.map(character => {
+  let content: JSX.Element[] = [];
+
+  characters?.forEach(character => {
     const isExpanded = character.id === expandedCharacterId;
-    return (
+    content.push(
       <CharacterShow
         key={character.id}
         character={character}
@@ -24,11 +26,20 @@ const CharacterList = () => {
         expand={isExpanded}
       />
     );
+    if (isExpanded) {
+      content.push(
+        <div
+          key={"detail-" + character.id}
+          className="col-span-3 row-span-1 bg-red-200">
+          <img src="https://placekitten.com/600/300" alt="kitty" />
+        </div>
+      );
+    }
   });
 
   return (
-    <div className='container mx-auto max-w-screen-xl px-4 pt-16 pb-16 lg:pt-4 lg:pb-4'>
-      <div className='grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 mt-2 lg:grid-cols-5 gap-4'>
+    <div className="container mx-auto max-w-screen-xl px-4 pt-16 pb-16 lg:pt-4 lg:pb-4">
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 mt-2 lg:grid-cols-5 gap-4">
         {content}
         {content}
         {content}
